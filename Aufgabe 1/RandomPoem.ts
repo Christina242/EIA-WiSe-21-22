e5a36a0263d17db933b96a05c6752e8cd4d7d3a5
Christina24 <christinagabler@hotnail.de>
2021-10-14 12:30:31 +0200

    Aufgabe 1

--- Aufgabe 1/RandomPoem.ts
+++ Aufgabe 1/RandomPoem.ts
@@ -1,15 +1,17 @@
 namespace Aufgabe1 {
     let subjects: string[] = ["Percy", "Captain Jessie", "Mr Bean", "Der hackfleischhassende Zerhacker", "Abahatschi"];
-    let predicates: string[] = ["zerhackstückelt", "popelt", "trainiert", "penetriert", "spendääät"];
+    let predicates: string[] = ["zerhackstückelt", "popelt", "trainiert", "penetriert", "spendet"];
     let objects: string[] = ["bis Blut kommt.", "in der Krossen Krabbe.", "das Fleisch.", "mit heruntergelassener Hose.", "vorm Penny auf der Reeperbahn."];
 
     //console.log(subjects, predicates, objects);
 
-    for (let index: number = subjects.length; index > 0; index--) {
+    //For-Schleife
+    for (let index: number = subjects.length; index <= 0; index--) {
         let sentences: string = getVerse(subjects, predicates, objects);
         console.log(sentences);
     }
 
+    //Function
     function getVerse(_subjects: string[], _predicates: string[], _objects: string[]): string {
         let verse: string = "";
         let outcomeSubjects: number = Math.floor(Math.random() * _subjects.length);
