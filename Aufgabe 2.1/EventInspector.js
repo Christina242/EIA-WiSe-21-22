8076c574768b55a8688500b978a7aa6e9528bc7f
Christina24 <christinagabler@hotnail.de>
2021-10-28 14:29:22 +0200

    Aufgabe 2.1

---
+++ Aufgabe 2.1/EventInspector.js
@@ -0,0 +1,34 @@
+var EventExercise;
+(function (EventExercise) {
+    window.addEventListener("load", handleLoad);
+    //1. Funktion erstellen
+    function handleLoad(_event) {
+        document.addEventListener("mouseMove", setInfoBox);
+        document.addEventListener("click", logInfo);
+        document.addEventListener("keyUp", logInfo);
+        //Install Listener auf body 
+        document.body.addEventListener("click", logInfo);
+        document.body.addEventListener("keyUp", logInfo);
+        //Install Listener auf Id
+        document.getElementById("div0").addEventListener("keyUp", logInfo);
+        document.getElementById("div0").addEventListener("click", logInfo);
+        document.getElementById("div1").addEventListener("keyUp", logInfo);
+        document.getElementById("div1").addEventListener("click", logInfo);
+    }
+    // zweite Funktion erstellen, Kooordinaten der
+    function setInfoBox(_event) {
+        var mousex = _event.x;
+        var mousey = _event.y;
+        document.getElementById("span").innerHTML = "Position X = " + mousex + ", Position Y = " + mousey;
+        document.getElementById("span").style.left = _event.x + "px";
+        document.getElementById("span").style.top = _event.y + "px";
+    }
+    //dritte Funktion erstellen
+    function logInfo(_event) {
+        console.log("Target: " + _event.target);
+        console.log("CurrentTarget: " + _event.currentTarget);
+        console.log("Type Event: " + _event.type);
+        console.log(_event);
+    }
+})(EventExercise || (EventExercise = {}));
+//# sourceMappingURL=EventInspector.js.map
---
+++ Aufgabe 2.1/EventInspector.ts
@@ -0,0 +1,42 @@
+namespace EventExercise {
+    window.addEventListener("load", handleLoad);
+    
+    //1. Funktion erstellen
+    function handleLoad(_event: Event): void {
+
+        document.addEventListener("mouseMove", setInfoBox);
+        document.addEventListener("click", logInfo);
+        document.addEventListener("keyUp", logInfo);
+
+        //Install Listener auf body 
+        document.body.addEventListener("click", logInfo);
+        document.body.addEventListener("keyUp", logInfo);
+
+        //Install Listener auf Id
+        document.getElementById("div0").addEventListener("keyUp", logInfo);
+        document.getElementById("div0").addEventListener("click", logInfo);
+
+        document.getElementById("div1").addEventListener("keyUp", logInfo);
+        document.getElementById("div1").addEventListener("click", logInfo);
+    }
+
+    // zweite Funktion erstellen, Kooordinaten der
+    function setInfoBox(_event: MouseEvent): void {
+        let mousex: number = _event.x;
+        let mousey: number = _event.y;
+
+        document.getElementById("span").innerHTML = "Position X = " + mousex + ", Position Y = " + mousey;
+
+        document.getElementById("span").style.left = _event.x + "px";
+        document.getElementById("span").style.top = _event.y + "px";
+    }
+
+    //dritte Funktion erstellen
+    function logInfo(_event: Event): void {
+            console.log("Target: " + _event.target);
+            console.log("CurrentTarget: " + _event.currentTarget);
+            console.log("Type Event: " + _event.type);
+            console.log(_event);
+    }}
+
+    // Leider zeigt mir mein Span keine Info an und ich hab es nicht geschaft herauszufinden warum :(
