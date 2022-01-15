6a5f1b81c74a22ddac1c73dbe50a75591f3c4d44
Christina24 <christinagabler@hotnail.de>
2022-01-15 12:16:54 +0100

    Aufgabe 11.1

---
+++ Aufgabe 11.1/Scripte/Moveable.js
@@ -0,0 +1,24 @@
+"use strict";
+var Aufgabe10_2;
+(function (Aufgabe10_2) {
+    class Moveable {
+        posX;
+        posY;
+        velocityX;
+        velocityY;
+        constructor(_position, _velocity) {
+            this.posX = _position.x;
+            this.posY = _position.y;
+            this.velocityX = _velocity.x;
+            this.velocityY = _velocity.y;
+        }
+        draw() {
+            //
+        }
+        update() {
+            //
+        }
+    }
+    Aufgabe10_2.Moveable = Moveable;
+})(Aufgabe10_2 || (Aufgabe10_2 = {}));
+//# sourceMappingURL=Moveable.js.map
